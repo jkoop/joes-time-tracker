6bde7c717227195ca0bbdba2bde532b69589b539
Joe Koop <joe@joekoop.com>
2021-07-02 13:02:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-// Modules to control application life and create native browser window
 const {app, ipcMain, BrowserWindow, Tray, Menu} = require('electron')
 const path = require('path')
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('db.sqlite');
 
 function createWindow (projectCount) {
-    // Create the browser window.
     const mainWindow = new BrowserWindow({
         width: 300,
         minWidth: 300,
@@ -25,17 +23,9 @@ function createWindow (projectCount) {
     });
 
     mainWindow.menuBarVisible = false;
-
-    // and load the index.html of the app.
     mainWindow.loadFile('index.html')
-
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
     // tray = new Tray(path.join(__dirname, './icons/clock-white.png'));
 
@@ -55,23 +45,14 @@ app.whenReady().then(() => {
 
     db.get('SELECT count(projectId) AS count FROM project WHERE isTrashed=0', function(err, row){
         db.close();
-        createWindow(row.count);
+        createWindow((row ?? []).count);
     });
 
     app.on('activate', function () {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-// require('./backend.js');
