30cd1a42e625f3d0fc81af7773b54f558c648016
Joe Koop <joe@joekoop.com>
2021-07-03 10:36:47 -0500

    Delete INSTALL.sh

--- INSTALL.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-git clone https://github.com/electron/electron-quick-start
-mv -n electron-quick-start/node* .
-rm -fr electron-quick-start
-npm install
-npm install jquery sqlite3
-npm start
